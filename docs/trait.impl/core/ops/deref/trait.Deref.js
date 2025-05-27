a9e0879dcb852b74ea7e0fe640de139f32d3a92a
Axect <axect.tg@proton.me>
2025-05-27 17:45:57 +0900

    v0.39.7

--- docs/trait.impl/core/ops/deref/trait.Deref.js
+++ docs/trait.impl/core/ops/deref/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["peroxide",[["impl&lt;T: <a class=\"trait\" href=\"peroxide/traits/math/trait.Vector.html\" title=\"trait peroxide::traits::math::Vector\">Vector</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"peroxide/traits/pointer/struct.Redox.html\" title=\"struct peroxide::traits::pointer::Redox\">Redox</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["peroxide",[["impl&lt;T: <a class=\"trait\" href=\"peroxide/traits/math/trait.Vector.html\" title=\"trait peroxide::traits::math::Vector\">Vector</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"peroxide/traits/pointer/struct.Redox.html\" title=\"struct peroxide::traits::pointer::Redox\">Redox</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
